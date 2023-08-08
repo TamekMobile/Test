ec8e2fcc7678f47ee329e2b8f6f3620d59d18c22
Kadir Şahin <99869817+KadirShn@users.noreply.github.com>
2023-08-08 11:07:37 +0300

    comitv.345

---
+++ klasör/dene.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const dene = () => {
+    //fşldjslfdsjlfdsjlf
+  return (
+    <View>
+      <Text>dene</Text>
+      
+    </View>
+  )
+}
+
+export default dene
